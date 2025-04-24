575bfd55095c7b10447feb507bb9e1e569902aba
William Hearn <william.hearn@ssc-spc.gc.ca>
2025-04-23 20:30:29 -0400

    feat(naming): Add project to name spec

--- encryption_set.tf
+++ encryption_set.tf
@@ -22,7 +22,7 @@ module "cluster_key_vault" {
 
   azure_resource_attributes = var.azure_resource_attributes
   naming_convention         = var.naming_convention
-  user_defined              = "cluster"
+  user_defined              = "CLUSTER"
 
   resource_group_name = azurerm_resource_group.secrets.name
 
